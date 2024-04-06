05ccbf19019c4cf2eb13038463e817bed5cadbe0
nguyenngocthuan11062004 <thuan.nn225413@sis.hust.edu.vn>
2024-04-06 12:51:18 +0700

    thuận thêm phần tấn công vào game

--- core/src/com/yourname/pacman/MyGdxGame.java
+++ core/src/com/yourname/pacman/MyGdxGame.java
@@ -12,85 +12,115 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
-    SpriteBatch hero;
+    SpriteBatch batch;
     Animation<TextureRegion> chayLen, chayXuong, chayTrai, chayPhai;
     Animation<TextureRegion> dungImLen, dungImXuong, dungImTrai, dungImPhai;
+    Animation<TextureRegion> attackUp, attackDown, attackLeft, attackRight;
     Animation<TextureRegion> hoatAnhHienTai;
     Texture anhChayLen, anhChayXuong, anhChayTrai, anhChayPhai;
     Texture anhDungImLen, anhDungImXuong, anhDungImTrai, anhDungImPhai;
-    float viTriX, viTriY, thoiGianTrangThai = 0;
-    float diChuyenX = 0, diChuyenY = 0;
-    float tocDo = 250;
+    Texture anhAttackUp, anhAttackDown, anhAttackLeft, anhAttackRight;
+    float viTriX, viTriY, thoiGianTrangThai = 0f;
+    float diChuyenX = 0f, diChuyenY = 0f;
+    float tocDo = 250f;
+    boolean isAttacking = false;
+
+    // Định nghĩa các hướng di chuyển
+    enum Direction {
+        UP, DOWN, LEFT, RIGHT, IDLE
+    }
+
+    // Biến lưu hướng di chuyển hiện tại
+    Direction currentDirection = Direction.IDLE;
 
     @Override
     public void create() {
-        hero = new SpriteBatch();
-        // Tải và khởi tạo hình ảnh và hoạt ảnh cho mỗi trạng thái di chuyển và đứng yên
-        anhChayLen = new Texture("runup.png");
-        anhChayXuong = new Texture("rundown.png");
-        anhChayTrai = new Texture("runleft.png");
-        anhChayPhai = new Texture("runright.png");
-
-        anhDungImLen = new Texture("idleup.png");
-        anhDungImXuong = new Texture("idledown.png");
-        anhDungImTrai = new Texture("idleleft.png");
-        anhDungImPhai = new Texture("idleright.png");
-
-        chayLen = taoHoatAnhTuAnh(anhChayLen, 6);
-        chayXuong = taoHoatAnhTuAnh(anhChayXuong, 6);
-        chayTrai = taoHoatAnhTuAnh(anhChayTrai, 6);
-        chayPhai = taoHoatAnhTuAnh(anhChayPhai, 6);
-
-        dungImLen = taoHoatAnhTuAnh(anhDungImLen, 1);
-        dungImXuong = taoHoatAnhTuAnh(anhDungImXuong, 1);
-        dungImTrai = taoHoatAnhTuAnh(anhDungImTrai, 1);
-        dungImPhai = taoHoatAnhTuAnh(anhDungImPhai, 1);
-
+        batch = new SpriteBatch();
+        initializeAnimations();
         hoatAnhHienTai = dungImXuong; // Mặc định bắt đầu với hướng đứng yên xuống
-
         viTriX = Gdx.graphics.getWidth() / 2 - 20;
         viTriY = Gdx.graphics.getHeight() / 2 - 20;
 
-        for (Controller dieuKhien : Controllers.getControllers()) {
-            dieuKhien.addListener(new ControllerAdapter() {
-                @Override
-                public boolean axisMoved(Controller dieuKhien, int chiSoTruc, float giaTri) {
-                    // Xử lý di chuyển dựa trên input từ controller
-                    xuLyDiChuyen(chiSoTruc, giaTri);
-                    return false;
+        Controllers.addListener(new ControllerAdapter() {
+            @Override
+            public boolean buttonDown(Controller controller, int buttonCode) {
+                if (buttonCode == 1) { // Giả sử mã cho nút X là 1
+                    isAttacking = true;
+                    return true;
                 }
-            });
-        }
+                return false;
+            }
+
+            @Override
+            public boolean buttonUp(Controller controller, int buttonCode) {
+                if (buttonCode == 1) {
+                    isAttacking = false;
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean axisMoved(Controller controller, int axisIndex, float value) {
+                if (Math.abs(value) > 0.2) { // Độ nhạy để tránh nhận diện nhầm những chuyển động nhỏ
+                    if (axisIndex == 0) { // Trục X
+                        diChuyenX = value * tocDo;
+                    } else if (axisIndex == 1) { // Trục Y
+                        diChuyenY = -value * tocDo; // Đảo ngược giá trị vì trục Y trên joystick ngược với trục Y trên màn hình
+                    }
+                    return true;
+                } else {
+                    if (axisIndex == 0) {
+                        diChuyenX = 0;
+                    } else if (axisIndex == 1) {
+                        diChuyenY = 0;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
-    private void xuLyDiChuyen(int chiSoTruc, float giaTri) {
-        boolean diChuyen = Math.abs(giaTri) > 0.2;
-        if (chiSoTruc == 0) { // Trục X
-            diChuyenX = diChuyen ? giaTri * tocDo : 0;
-        } else if (chiSoTruc == 1) { // Trục Y
-            diChuyenY = diChuyen ? -giaTri * tocDo : 0;
-        }
+    private void initializeAnimations() {
+        // Khởi tạo và cấu hình animations ở đây
+        anhChayLen = new Texture(Gdx.files.internal("runup.png"));
+        anhChayXuong = new Texture(Gdx.files.internal("rundown.png"));
+        anhChayTrai = new Texture(Gdx.files.internal("runleft.png"));
+        anhChayPhai = new Texture(Gdx.files.internal("runright.png"));
 
-        // Cập nhật hoạt ảnh dựa trên hướng di chuyển
-        if (diChuyenX < 0) hoatAnhHienTai = chayTrai;
-        else if (diChuyenX > 0) hoatAnhHienTai = chayPhai;
-        else if (diChuyenY > 0) hoatAnhHienTai = chayLen;
-        else if (diChuyenY < 0) hoatAnhHienTai = chayXuong;
-        else { // Đứng yên
-            if (hoatAnhHienTai == chayTrai || hoatAnhHienTai == dungImTrai) hoatAnhHienTai = dungImTrai;
-            else if (hoatAnhHienTai == chayPhai || hoatAnhHienTai == dungImPhai) hoatAnhHienTai = dungImPhai;
-            else if (hoatAnhHienTai == chayLen || hoatAnhHienTai == dungImLen) hoatAnhHienTai = dungImLen;
-            else if (hoatAnhHienTai == chayXuong || hoatAnhHienTai == dungImXuong) hoatAnhHienTai = dungImXuong;
-        }
+        anhDungImLen = new Texture(Gdx.files.internal("idleup.png"));
+        anhDungImXuong = new Texture(Gdx.files.internal("idledown.png"));
+        anhDungImTrai = new Texture(Gdx.files.internal("idleleft.png"));
+        anhDungImPhai = new Texture(Gdx.files.internal("idleright.png"));
+
+        anhAttackUp = new Texture(Gdx.files.internal("attackup.png"));
+        anhAttackDown = new Texture(Gdx.files.internal("attackdown.png"));
+        anhAttackLeft = new Texture(Gdx.files.internal("attackleft.png"));
+        anhAttackRight = new Texture(Gdx.files.internal("attackright.png"));
+
+        chayLen = createAnimation(anhChayLen, 6);
+        chayXuong = createAnimation(anhChayXuong, 6);
+        chayTrai = createAnimation(anhChayTrai, 6);
+        chayPhai = createAnimation(anhChayPhai, 6);
+
+        dungImLen = createAnimation(anhDungImLen, 4);
+        dungImXuong = createAnimation(anhDungImXuong, 4);
+        dungImTrai = createAnimation(anhDungImTrai, 4);
+        dungImPhai = createAnimation(anhDungImPhai, 4);
+
+        attackUp = createAnimation(anhAttackUp, 7);
+        attackDown = createAnimation(anhAttackDown, 7);
+        attackLeft = createAnimation(anhAttackLeft, 7);
+        attackRight = createAnimation(anhAttackRight, 7);
     }
 
-    private Animation<TextureRegion> taoHoatAnhTuAnh(Texture anh, int soKhungHinh) {
-        TextureRegion[][] tmpFrames = TextureRegion.split(anh, 40, 40);
-        TextureRegion[] hoatAnhFrames = new TextureRegion[soKhungHinh];
-        for (int i = 0; i < soKhungHinh; i++) {
-            hoatAnhFrames[i] = tmpFrames[0][i];
+    private Animation<TextureRegion> createAnimation(Texture texture, int frameCount) {
+        TextureRegion[][] tmpFrames = TextureRegion.split(texture, texture.getWidth() / frameCount, texture.getHeight());
+        TextureRegion[] animationFrames = new TextureRegion[frameCount];
+        for (int i = 0; i < frameCount; i++) {
+            animationFrames[i] = tmpFrames[0][i];
         }
-        return new Animation<>(0.1f, hoatAnhFrames);
+        return new Animation<>(0.1f, animationFrames);
     }
 
     @Override
@@ -99,19 +129,96 @@ public class MyGdxGame extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         thoiGianTrangThai += Gdx.graphics.getDeltaTime();
-        TextureRegion khungHienTai = hoatAnhHienTai.getKeyFrame(thoiGianTrangThai, true);
-        
-        viTriX += diChuyenX * Gdx.graphics.getDeltaTime();
-        viTriY += diChuyenY * Gdx.graphics.getDeltaTime();
+        updateGameState();
+        updateAnimationState();
 
-        hero.begin();
-        hero.draw(khungHienTai, viTriX, viTriY, 80, 80); // Vẽ nhân vật với kích thước gấp đôi
-        hero.end();
+        batch.begin();
+        TextureRegion khungHienTai = hoatAnhHienTai.getKeyFrame(thoiGianTrangThai, true);
+        batch.draw(khungHienTai, viTriX, viTriY, khungHienTai.getRegionWidth() * 2, khungHienTai.getRegionHeight() * 2);
+        batch.end();
+    }
+
+    private void updateGameState() {
+        // Cập nhật trạng thái game, bao gồm vị trí nhân vật
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        viTriX += diChuyenX * deltaTime;
+        viTriY += diChuyenY * deltaTime;
+
+        // Cập nhật hướng di chuyển dựa trên di chuyển X và Y
+        if (diChuyenX < 0) {
+            currentDirection = Direction.LEFT;
+        } else if (diChuyenX > 0) {
+            currentDirection = Direction.RIGHT;
+        } else if (diChuyenY > 0) {
+            currentDirection = Direction.UP;
+        } else if (diChuyenY < 0) {
+            currentDirection = Direction.DOWN;
+        } else if (diChuyenX == 0 && diChuyenY == 0 && !isAttacking) {
+            currentDirection = Direction.IDLE;
+        }
+
+        viTriX = Math.max(0, Math.min(viTriX, Gdx.graphics.getWidth() - 40));
+        viTriY = Math.max(0, Math.min(viTriY, Gdx.graphics.getHeight() - 40));
+    }
+
+    private void updateAnimationState() {
+        if (isAttacking) {
+            switch (currentDirection) {
+                case UP:
+                    hoatAnhHienTai = attackUp;
+                    break;
+                case DOWN:
+                    hoatAnhHienTai = attackDown;
+                    break;
+                case LEFT:
+                    hoatAnhHienTai = attackLeft;
+                    break;
+                case RIGHT:
+                    hoatAnhHienTai = attackRight;
+                    break;
+                case IDLE:
+                default:
+                    // Nếu nhân vật đứng yên khi bắt đầu tấn công, có thể giữ nguyên hướng tấn công cuối cùng
+                    // hoặc đặt một hướng mặc định
+                    hoatAnhHienTai = attackDown;
+                    break;
+            }
+        } else {
+            // Xử lý hướng di chuyển và chọn hoạt ảnh phù hợp
+            if (diChuyenX < 0) {
+                hoatAnhHienTai = chayTrai;
+            } else if (diChuyenX > 0) {
+                hoatAnhHienTai = chayPhai;
+            } else if (diChuyenY > 0) {
+                hoatAnhHienTai = chayLen;
+            } else if (diChuyenY < 0) {
+                hoatAnhHienTai = chayXuong;
+            } else {
+                switch (currentDirection) {
+                    case UP:
+                        hoatAnhHienTai = dungImLen;
+                        break;
+                    case DOWN:
+                        hoatAnhHienTai = dungImXuong;
+                        break;
+                    case LEFT:
+                        hoatAnhHienTai = dungImTrai;
+                        break;
+                    case RIGHT:
+                        hoatAnhHienTai = dungImPhai;
+                        break;
+                    case IDLE:
+                    default:
+                        hoatAnhHienTai = dungImXuong; // Hoặc giữ nguyên hướng đứng yên cuối cùng
+                        break;
+                }
+            }
+        }
     }
 
     @Override
     public void dispose() {
-        hero.dispose();
+        batch.dispose();
         anhChayLen.dispose();
         anhChayXuong.dispose();
         anhChayTrai.dispose();
@@ -120,5 +227,9 @@ public class MyGdxGame extends ApplicationAdapter {
         anhDungImXuong.dispose();
         anhDungImTrai.dispose();
         anhDungImPhai.dispose();
+        anhAttackUp.dispose();
+        anhAttackDown.dispose();
+        anhAttackLeft.dispose();
+        anhAttackRight.dispose();
     }
 }
--- desktop/src/com/yourname/pacman/DesktopLauncher.java
+++ desktop/src/com/yourname/pacman/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("dungeon game team");
+		config.setTitle("dungeon game project");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
